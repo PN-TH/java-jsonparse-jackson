32f7c86df0a0f185218d6bf29be8786ad5b3ea65
PN-TH <tponthoreau@gmail.com>
2020-02-24 17:06:17 +0100

    end

--- src/main/Parse.java
+++ src/main/Parse.java
@@ -14,20 +14,21 @@ public class Parse {
             // write your code here !
 
             // TODO : get the root from the file JSON_WEATHER_PATH
-            JsonNode root = null;
+            JsonNode root = objectMapper.readTree(new File("./weather.json"));
 
             // TODO : get the value of "name" attribute
-            String cityName = null;
+            String cityName = root.get("name").asText();
 
             // TODO : get the "lat" and "lon" values of the "coord"
-            Double cityLatitude = null;
-            Double cityLongitude = null;
+            JsonNode coordObject = root.get("coord");
+            Double cityLatitude = coordObject.get("lat").asDouble();
+            Double cityLongitude = coordObject.get("lon").asDouble();
 
             // TODO : get the "wind" attribute as an Wind object
-            Wind wind = null;
+            Wind wind = objectMapper.convertValue(root.get("wind"), Wind.class);
 
             // TODO : get the "weather" attribute as an array of Weather objects
-            Weather[] weathers = {};
+            Weather[] weathers = objectMapper.convertValue(root.get("weather"), Weather[].class);
 
             // Don't touch this !
             System.out.printf("City name: %s%n", cityName);
@@ -47,7 +48,7 @@ public class Parse {
                 Weather infos: src.main.Weather{id=300, main='Drizzle', description='light intensity drizzle', icon='09d'}
                 Weather infos: src.main.Weather{id=800, main='Clear', description='clear sky', icon='01n'}
             */
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
